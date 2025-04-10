7c9998c5420a031ade1950c42cd3302a577e0871
LuckyDucky0305 <caitlintze@gmail.com>
2025-04-09 22:23:32 -0500

    basic functions
    started backend and created basic functions of program (viewing account balance, deposit, withdrawal, creation, deletion, modification).

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-
+"""
 import tkinter as tk
 
 # Create the main window
 window = tk.Tk()
-window.title("Caitlin Bank")
+window.title("Caitlin's Bank")
 
 # Add a label
 welcome = tk.Label(window, text="Welcome to your bank account!")
 choice1 = tk.Button(window, text="1) View account balance")
-choice2 = tk.Button(window, text="2) Make withdrawal")
-choice3 = tk.Button(window, text="3) Make deposit")
-choice4 = tk.Button(window, text="4) Exit")
+choice2 = tk.Button(window, text="2) Make a deposit")
+choice3 = tk.Button(window, text="3) Create a new account")
+choice4 = tk.Button(window, text="4) Delete an account")
+choice5 = tk.Button(window, text="5) Modify account details")
 
 welcome.pack()
 
@@ -19,7 +20,87 @@ choice1.pack()
 choice2.pack()
 choice3.pack()
 choice4.pack()
-
+choice5.pack()
 
 # Start the Tkinter event loop
 window.mainloop()
+
+"""
+balance = 0
+uName = ""
+pWord = ""
+email = "" 
+
+def viewAccount():
+    if hasAccount():
+        print("----------------------")
+        print("Your balance: " + balance)
+        print("----------------------")
+
+
+def deposit():
+    if hasAccount():
+        deposit = input("How much would you like to deposit? ")
+        balance += int(deposit)
+        viewAccount()
+
+def withdrawal():
+    if hasAccount():
+        withdraw = input("How much would you like to withdraw? ")
+        balance -= withdraw
+        viewAccount()
+
+def createAccount():
+    print("\nWelcome! To create a new account, please enter the following information... ")
+    uName = input("New username: ")
+    pWord = input("Password: ")
+    email = input("Email: ")
+    print("You've successfully created a new account!")
+
+def deleteAccount():
+    if hasAccount():
+        confirmation = input("Are you sure you want to delete your account? (y)")
+        if confirmation.upper == "y":
+            print("Deleted")
+
+def modifyAccount():
+    if hasAccount():
+        print("Username: " + uName)
+        for i in len(pWord):
+            password += "*"
+        print("Password: " + password)
+        print("Email: " + email)
+        choice = input("What would you like to modify? ")
+
+def hasAccount():
+    if uName == "":
+        print("You don't have an account yet. Please choose option 4 to create an account to get started.")
+        return False
+    else:
+        return True
+    
+print("Welcome to your bank account!")
+while True:
+    print("\nCaitlin's bank -------------")
+    print("1) View account balance")
+    print("2) Make a deposit")
+    print("3) Make a withdrawal")
+    print("4) Create a new account")
+    print("5) Delete an account")
+    print("6) Modify account details")
+    choice = input("What would you like to do? ")
+
+    if choice == "1":
+        viewAccount()
+    elif choice == "2": 
+        deposit()
+    elif choice == "3": 
+        withdrawal()
+    elif choice == "4": 
+        createAccount()
+    elif choice == "5": 
+        deleteAccount()
+    elif choice == "6": 
+        modifyAccount()
+    print(uName)
+
